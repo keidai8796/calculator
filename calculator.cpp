56fbf20914562317f7d48ea9cfdf9f834933bff1
keidai <keidai8796@gmail.com>
2022-05-08 11:44:45 +0900

    update calculator.cpp Enabled to calculate addition and subtraction

---
+++ calculator.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef string::const_iterator State;
+
+double expression(State &begin);
+double number(State &begin);
+void error();
+
+void error(){
+    cout<<"error"<<endl;
+    exit(0);
+}
+
+double expression(State &begin){
+    bool minus=false;
+    if((*begin)=='-'){
+        minus=true;
+        begin++;
+    }
+    double ret=number(begin);
+    if(minus)ret*=-1;
+    while(1){
+        if((*begin)=='+'){
+            begin++;
+            ret+=number(begin);
+        }
+        else if((*begin)=='-'){
+            begin++;
+            ret-=number(begin);
+        }
+        else break;
+    }
+    return ret;
+}
+
+double number(State &begin){
+    double ret=0;
+    while(0<=((*begin)-'0')&&((*begin)-'0')<=9){
+        ret*=10;
+        ret+=(*begin)-'0';
+        begin++;
+    }
+    return ret;
+}
+
+int main(){
+    string formula;
+    getline(cin,formula);
+    formula.erase(remove(formula.begin(),formula.end(),' '),formula.end());
+    State begin=formula.begin();
+    double ret=expression(begin);
+    if(begin!=formula.end())error();
+    cout<<ret<<endl;
+}
